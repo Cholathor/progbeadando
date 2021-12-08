ed9c87629da7f68468999bcfc20a51bd9aca5de4
Cholathor <klenczner.marton@gmail.com>
2021-12-08 20:47:23 +0100

    Javított fogyasztó megjelenítés

--- beadando2proba/mainwindow.cpp
+++ beadando2proba/mainwindow.cpp
@@ -7,7 +7,17 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    mezok = QVector<QVector<Mezo>>(10, QVector<Mezo>(10, Mezo(0, 0, 0, "")));
+    size = 11;
+    mezok = QVector<QVector<Mezo>>(size, QVector<Mezo>(size, Mezo(0, 0, 0, "")));
+    szinek = {
+        {"R", QColor(Qt::red)},
+        {"G", QColor(Qt::green)},
+        {"B", QColor(Qt::blue)},
+        {"RG", QColor(Qt::yellow)},
+        {"RB", QColor(Qt::darkMagenta)},
+        {"BG", QColor(Qt::cyan)},
+        {"RGB", QColor(Qt::white)}
+    };
 }
 
 MainWindow::~MainWindow()
@@ -17,62 +27,64 @@ MainWindow::~MainWindow()
 
 void MainWindow::refresh()
 {
-    ui->tableWidget->setColumnCount(0);
-    ui->tableWidget->setRowCount(0);
+    int field_size = 40;
 
-    int size = 50;
-    ui->tableWidget->setIconSize(QSize(size, size));
-
-    for (int i = 0; i < mezok.size(); i++)
+    for (int j = 0; j < size; j++)
     {
-        ui->tableWidget->insertRow(ui->tableWidget->rowCount());
-        ui->tableWidget->setRowHeight(ui->tableWidget->rowCount()-1, size);
-    }
-    for (int i = 0; i < mezok[0].size(); i++)
-    {
-
-        ui->tableWidget->insertColumn(ui->tableWidget->columnCount());
-        ui->tableWidget->setColumnWidth(ui->tableWidget->columnCount()-1, size);
-    }
-    for (int j = 0; j < mezok.size(); j++)
-    {
-        for (int i = 0; i < mezok[j].size(); i++)
+        for (int i = 0; i < size; i++)
         {
-            if (mezok[i][j].get_id() != 0)
+            if (mezok[i][j].get_id() == 0)
             {
+                QLabel *k = new QLabel();
 
-                QIcon icon(mezok[i][j].get_image());
-                QTableWidgetItem *icon_item = new QTableWidgetItem;
-
-                icon_item->setIcon(icon);
-                ui->tableWidget->setItem(i, j, icon_item);
+                ui->gridLayout->addWidget(k, i, j);
+                QPixmap m(1, 1);
+                m.fill(Qt::gray);
+                k->setPixmap(m.scaled(field_size, field_size));
+            } else if (mezok[i][j].get_id() < 5)
+            {
+                draw_arrow(i, j, mezok[i][j].get_image(), field_size);
+            } else if (mezok[i][j].get_id() == 5)
+            {
+                draw_fogyaszto(i, j, mezok[i][j].sink);
             }
         }
     }
-    ui->tableWidget->horizontalHeader()->setVisible(false);
-    ui->tableWidget->verticalHeader()->setVisible(false);
+}
+void MainWindow::draw_arrow(int a, int b, QString filename, int field_size)
+{
+    QLabel *k = new QLabel();
+
+    ui->gridLayout->addWidget(k, a, b);
+    QPixmap m(1, 1);
+    m.load(filename);
+    k->setPixmap(m.scaled(field_size, field_size));
+}
+void MainWindow::draw_fogyaszto(int a, int b, Fogyaszto f)
+{
+    QGridLayout *g = new QGridLayout();
+    int i = 0;
+    for (auto szin: f.osszes_szin)
+    {
+        QLabel *k = new QLabel();
+        g->addWidget(k, i, 0);
+
+        QPixmap m (1, 1);
+        m.fill(szinek[szin.first]);
+        k->setPixmap(m.scaled(szin.second, 3));
+        i++;
+    }
+
+
+    ui->gridLayout->addLayout(g, a, b);
+
+    qDebug() << g->rowCount();
 }
 
 void MainWindow::on_pushButton_clicked()
 {
    refresh();
 }
-
-
-void MainWindow::on_tableWidget_cellDoubleClicked(int row, int column)
-{
-    int a = ui->spinBox->value();
-    QString filename = "C:/Datafiles/progbeadandokepek/futoszalag" + QString::number(a) + ".png";
-    mezok[row][column].clicked(filename, a);
-    refresh();
-}
-
-
-void MainWindow::on_tableWidget_cellClicked(int row, int column)
-{
-    qDebug() << mezok[row][column].get_id();
-}
-
 
 double dist(int y1, int x1, int y2, int x2)
 {
@@ -114,7 +126,7 @@ void MainWindow::shortest_path(int y1, int x1, int y2, int x2)
         uj_x = x1+1;
         uj_id = 4;
     }
-//    int a = ui->spinBox->value();
+
     QString filename = "C:/Datafiles/progbeadandokepek/futoszalag" + QString::number(uj_id) + ".png";
     mezok[y1][x1].clicked(filename, uj_id);
     if (uj_x == x2 && uj_y == y2) return;
@@ -123,9 +135,26 @@ void MainWindow::shortest_path(int y1, int x1, int y2, int x2)
 
 void MainWindow::on_pushButton_2_clicked()
 {
-//    shortest_path(1, 1, 7, 7);
     shortest_path(9, 3, 3, 7);
-
     refresh();
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+//    Mezo m = Mezo(10, 10, 5, "");
+//    m.sink.vart_szin_hozzaad({"R", 10});
+//    m.sink.vart_szin_hozzaad({"RG", 15});
+//    m.sink.vart_szin_hozzaad({"B", 30});
+//    mezok[10][10] = m;
+//    refresh();
+    qDebug() << ui->gridLayout->rowCount();
+    clear_grid();
+    qDebug() << ui->gridLayout->rowCount();
+}
+
+void MainWindow::clear_grid()
+{
+    ui->gridLayout = new QGridLayout();
 }
 
--- beadando2proba/mainwindow.h
+++ beadando2proba/mainwindow.h
@@ -4,15 +4,52 @@
 #include <QMainWindow>
 #include <QVector>
 #include <QIcon>
+#include <QLabel>
+#include <map>
+#include <set>
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+class Fogyaszto
+{
+public:
+    std::map<QString, int> osszes_szin;
+
+    Fogyaszto(std::map<QString, int> osszes)
+    {
+        osszes_szin = osszes;
+    }
+
+    void vart_szin_hozzaad(std::pair<QString, int> uj_szin)
+    {
+        osszes_szin.insert(uj_szin);
+    }
+
+    void szin_bemegy(QString szin)
+    {
+        for (auto f: osszes_szin)
+        {
+            if (f.first == szin && f.second > 0)
+            {
+                osszes_szin[f.first]--;
+            }
+        }
+    }
+
+    Fogyaszto()
+    {
+        osszes_szin = std::map<QString, int>();
+    }
+};
+
 class Mezo
 {
     int x, y;
     QString kep;
     int id;
 public:
+    Fogyaszto sink;
 
     Mezo(int x_, int y_, int id_, QString kep_)
     {
@@ -20,9 +57,11 @@ public:
         y = y_;
         id = id_;
         kep = kep_;
+        sink = Fogyaszto();
     }
     Mezo()
     {
+        sink = Fogyaszto();
         id = 0;
     }
 
@@ -41,6 +80,8 @@ public:
         id = id_;
         kep = kep_;
     }
+
+
 };
 
 class MainWindow : public QMainWindow
@@ -54,17 +95,20 @@ public:
 private slots:
     void on_pushButton_clicked();
 
-    void on_tableWidget_cellDoubleClicked(int row, int column);
-
-    void on_tableWidget_cellClicked(int row, int column);
-
     void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
 
 private:
     Ui::MainWindow *ui;
     QVector<QVector<Mezo>> mezok;
+    int size;
+    std::map<QString, QColor> szinek;
 
     void shortest_path(int x1, int y1, int x2, int y2);
     void refresh();
+    void draw_arrow(int a, int b, QString filename, int field_size);
+    void draw_fogyaszto(int a, int b, Fogyaszto f);
+    void clear_grid();
 };
 #endif // MAINWINDOW_H
